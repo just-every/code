36a7f9b8298c3a5936d1618185fd15284e54c59b
Oleg Lebedev <oleg@lebedev.au>
2025-10-11 23:00:58 +1100

    fix: nix build

--- code-rs/default.nix
+++ code-rs/default.nix
@@ -1,17 +1,32 @@
-{ pkgs, monorep-deps ? [], ... }:
+{ pkgs, monorep-deps ? [ ], ... }:
 let
   env = {
     PKG_CONFIG_PATH = "${pkgs.openssl.dev}/lib/pkgconfig:$PKG_CONFIG_PATH";
   };
+  codeRsSrc = pkgs.lib.cleanSource ./.;
+  codexSrc = pkgs.lib.cleanSource ../codex-rs;
+  workspaceSrc = pkgs.runCommand "code-rs-src" { } ''
+    mkdir $out
+    cp -r ${codeRsSrc} $out/code-rs
+    cp -r ${codexSrc} $out/codex-rs
+    chmod -R u+w $out
+  '';
 in
 rec {
   package = pkgs.rustPlatform.buildRustPackage {
     inherit env;
     pname = "code-rs";
     version = "0.1.0";
-    cargoLock.lockFile = ./Cargo.lock;
+    src = workspaceSrc;
+    cargoRoot = "code-rs";
+    buildAndTestSubdir = "code-rs";
+    cargoLock = {
+      lockFile = ./Cargo.lock;
+      outputHashes = {
+        "ratatui-0.29.0" = "sha256-HBvT5c8GsiCxMffNjJGLmHnvG77A6cqEL+1ARurBXho=";
+      };
+    };
     doCheck = false;
-    src = ./.;
     nativeBuildInputs = with pkgs; [
       pkg-config
       openssl
@@ -31,12 +46,12 @@ rec {
     ];
     shellHook = ''
       echo "Entering development shell for code-rs"
-      alias codex="cd ${package.src}/tui; cargo run; cd -"
+      alias code="cd ${package.src}/code-rs/tui; cargo run; cd -"
       ${pkgs.rustPlatform.cargoSetupHook}
     '';
   };
   app = {
     type = "app";
-    program = "${package}/bin/codex";
+    program = "${package}/bin/code";
   };
 }
